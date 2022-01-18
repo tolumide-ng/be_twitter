e8f43965a10d117ff065664f56958ce2c5c96532
tolumide-ng <tolumideshopein@gmail.com>
2022-01-18 16:36:15 +0100

    wrap hyper http client with tls implementation from hyper_tls

--- src/app/client.rs
+++ src/app/client.rs
@@ -1,17 +1,21 @@
+use hyper::{Client, client::{HttpConnector}, Method, Request, Body};
+use hyper_tls::HttpsConnector;
+use url::Url;
 use urlencoding::encode;
 use uuid::Uuid;
-use url::Url;
-use hyper::{Client, client::{HttpConnector, ResponseFuture}, Method, Request, Body};
+
 
 use crate::setup::variables::SettingsVars;
 
-struct AppClient {
-    pool: Client<HttpConnector>
+pub struct AppClient {
+    pool: Client<HttpsConnector<HttpConnector>>
 }
+
 
 impl AppClient {
     pub fn new() -> Self {
-        let pool = Client::new();
+        let https = HttpsConnector::new();
+        let pool = Client::builder().build::<_, hyper::Body>(https);
         
         Self {
             pool
@@ -19,19 +23,24 @@ impl AppClient {
 
     }
 
-    pub fn get_request_token(&self) -> ResponseFuture {
+
+    pub async fn make_call() {}
+
+    async fn get_oauth_request_token(&self) {
         let client = &self.pool.clone();
 
-        let SettingsVars { client_id, response_type, base_url, redirect_uri, 
-            code_challenge, state, client_secret, app_address, api_key, .. } = SettingsVars::new();
+        let SettingsVars { app_address, api_key, .. } = SettingsVars::new();
 
-        let callback_url = encode(app_address.as_str());
         let oauth_nonce = base64::encode(Uuid::new_v4().to_string());
+        let callback_url = encode(app_address.as_str());
 
-        let url = Url::parse_with_params(
+        let _url = Url::parse_with_params(
             "https://api.twitter.com/oauth/request_token", &[
                 ("oauth_callback", callback_url),
             ]).unwrap();
+
+
+        
         
         let req = Request::builder()
             .method(Method::POST)
@@ -39,8 +48,14 @@ impl AppClient {
             .header("Authorization", format!("OAuth oauth_consumer_key={}", api_key))
             .header("oauth_nonce", oauth_nonce)
             .body(Body::from("Hello"))
+            // .await()
             .expect("");
 
-        client.request(req)
+        let ab = client.request(req).await;
+    }
+
+
+    pub async fn get_oauth_authorize(&self) {
+        let client = &self.pool.clone();
     }
 }
