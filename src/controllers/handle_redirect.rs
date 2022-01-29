b094c84fda50ee3449326f5e237b5973b129d7db
tolumide-ng <tolumideshopein@gmail.com>
2022-01-29 19:18:45 +0100

    Initial method to parse queryparams in redirect uri into AccessToken struct

--- src/controllers/handle_redirect.rs
+++ src/controllers/handle_redirect.rs
@@ -1,14 +1,43 @@
+use std::collections::HashMap;
+
 use hyper::{Body, Response, StatusCode, Request};
 use redis::{Client as RedisClient};
-use crate::{helpers::{response::{ApiResponse, ApiResponseBody}, request::{get_param_from_uri, HyperClient}}, setup::variables::SettingsVars};
+use crate::{helpers::{response::{ApiResponse, ApiResponseBody}, request::{get_param_from_uri, HyperClient}, keyval::KeyVal}, setup::variables::SettingsVars};
 
+struct AccessToken {
+    pub state: String,
+    pub  code: String,
+}
 
+impl AccessToken {
+    pub fn new(token: Option<HashMap<String, String>>) -> Self {
+        match token {
+            Some(token) => {
+                if token.contains_key("state") && token.contains_key("code") {
+                    return AccessToken {
+                        state: token.get("state").unwrap().to_string(),
+                        code: token.get("code").unwrap().to_string(),
+                    }
+                }
+                panic!("")
+            },
+            None => panic!("Invalid AccessToken")
+        }
+    }
+}
+
+// struct UriParams(String);
+
+// impl UriParams {
+//     pub fn new() -> Self {}
+// }
 
 // todo() - I should move all the controllers used to handle 2.0 authentication into one struct and represent them as methods within the struct
 pub async fn handle_redirect(req: Request<hyper::Body>, hyper_client: &HyperClient, redis_client: RedisClient) -> ApiResponse {
     let SettingsVars{state, ..} = SettingsVars::new();
 
     let query_params = get_param_from_uri(req.uri());
+    // let act = KeyVal::to_access_token(query_params.unwrap());
     if let Some(dict) = query_params {
         //todo() find a way to confirm if code and state are present in the dic, handle the edge cases
         let obtained_state = dict.get("state").unwrap();
--- src/helpers/keyval.rs
+++ src/helpers/keyval.rs
@@ -1,9 +1,16 @@
 use std::{collections::HashMap, borrow::Cow};
 use derive_more;
+use http::Uri;
 
 #[derive(Debug, derive_more::Deref, derive_more::DerefMut, derive_more::From, Clone, Default)]
 pub struct KeyVal(HashMap<Cow<'static, str>, Cow<'static, str>>);
 
+
+#[derive(Debug, Clone)]
+pub struct AccessToken {
+    pub state: String,
+    pub  code: String,
+}
 
 impl KeyVal {
     pub fn new() -> Self {
@@ -19,10 +26,10 @@ impl KeyVal {
         for (k, v) in list {
             self.insert(k.into(), v.into());
         }
-        // let ab = list.into_iter()
-        //     .map(|(k, v)| self.insert(k.into(), v.into())).collect();
         self
     }
+
+    // pub fn from_uri_param_to_dict(uri: &Uri) -> Self {}
 
     pub fn to_urlencode(&self) -> String {
         self.iter()
@@ -30,7 +37,37 @@ impl KeyVal {
             .collect::<Vec<_>>().join("&")
     }
 
+    // pub fn to_access_token(&self) -> Result<AccessToken, ValidationError> {
+    //     if self.contains_key("state") && self.contains_key("code") {
+    //         let at = AccessToken {
+    //             state: self.get("state").unwrap().to_string(),
+    //             code: self.get("code").unwrap().to_string()
+    //         };
+
+    //         return Ok(at)
+    //     }
+    //     return ValidationError("State or Code in missen in AccessToken".to_string())
+    //     // panic!("Invalid AccessToken")
+    // }
+
     // pub fn to_query_params(&self) -> String {
     //     self.iter().map(|(k, v)| forma)
     // }
+}
+
+#[derive(Debug, Clone)]
+struct ValidationError(String);
+
+impl std::fmt::Display for ValidationError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self)
+    }
+}
+
+
+impl std::error::Error for ValidationError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        // Compiler transparently casts RedisError to dyn::Error
+        Some(self)
+    }
 }
