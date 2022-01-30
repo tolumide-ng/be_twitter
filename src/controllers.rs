c825b2e02ef61c993ca6db47027c795d5d6ea8b0
tolumide-ng <tolumideshopein@gmail.com>
2022-01-31 00:51:43 +0100

    Revoke token controller

--- src/controllers.rs
+++ src/controllers.rs
@@ -3,9 +3,10 @@ mod not_found;
 mod authorize_bot;
 mod health_check;
 pub mod handle_redirect;
-mod access_token;
+mod revoke_token;
 
 pub use not_found::not_found;
 pub use authorize_bot::authorize_bot;
 pub use health_check::health_check;
 pub use handle_redirect::handle_redirect;
+pub use revoke_token::revoke_token;
