dfc9136b75e505557d6fe2ac47ac7ae8398f565c
tolumide-ng <tolumideshopein@gmail.com>
2022-04-30 14:20:39 +0100

    Modifies type of argument received by parse method

--- twitar/src/helpers/commons.rs
+++ twitar/src/helpers/commons.rs
@@ -25,9 +25,9 @@ impl std::fmt::Display for GrantType {
 pub struct UserId(#[display(fmt = "0")] Uuid,);
 
 impl UserId {
-    pub fn parse(input: Option<&String>) -> TResult<Self> {
+    pub fn parse(input: Option<String>) -> TResult<Self> {
         if let Some(id) = input {
-            let user_id = Uuid::parse_str(id)?;
+            let user_id = Uuid::parse_str(&id)?;
             return Ok(Self(user_id))
         }
         
